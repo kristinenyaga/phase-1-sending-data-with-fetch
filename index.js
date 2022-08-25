58ae12c11d05bff7120c1e18141a57e96cb2add8
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-26 01:43:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+let form=document.querySelector('form')
+const body=document.querySelector("body")
+form.addEventListener("submit", (e) =>{
+  e.preventDefault()
+  var name = document.getElementById("username").value;
+  var email= document.getElementById("email").value;
+  submitData(name,email)
+  
+  
+})
+function submitData(name,email){
+  fetch('http://localhost:3000/users',{
+    method:'POST',
+    headers:{
+    "Content-Type": "application/json",
+    Accept: "application/json"
+    },
+    body:JSON.stringify({
+      name:name,
+      email:email
+      
+    })
+  })
+  .then(res => res.json())
+  .then(userinput => {
+    body.append(userinput.id)
+  })
+  .catch(function (err) {
+   body.append(err.message)
+   
+  })
+}
